474b4703e1c4974894493190ac3e32b7f3d4ea82
parasellia7975 <pathulith@gmail.com>
2020-01-30 13:18:38 -0800

    Just Finished Unit00

---
+++ Unit00/src/AsciiArt.java
@@ -0,0 +1,32 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name - Athulith Paraselli
+//Date - 1/20/20
+//Class - APCSA
+//Lab  -
+
+public class AsciiArt
+{
+	public static void main ( String[] args )
+	{
+		System.out.println("Athulith Paraselli \n\n" );
+		
+		System.out.println("Dog" );
+		System.out.println("\n\n\n\n" );
+
+		System.out.println("                .-._ " );
+		System.out.println("               {_}^ )o" );
+		System.out.println("      {\\________//~`" );
+		System.out.println("       (         )" );
+		System.out.println("       /||~~~~~||\\");
+		System.out.println("      |_\\\\_    \\\\_\\_");
+		//add other output
+
+		System.out.println(" \n\n\n\nHelpFul Hints" );
+		System.out.println("\\\n" );
+		
+		System.out.println("\" \n" );
+		System.out.println("\'\n" ); 
+	}
+}
---
+++ Unit00/src/AsciiBox.java
@@ -0,0 +1,27 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -Athulith Paraselli
+//Date - 1/30/20
+//Class - APCSA
+//Lab  -
+
+public class AsciiBox
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Athulith Paraselli \t  1\\30\\20 \n\n" );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+                       + " );
+		System.out.println("+                       + " );
+		System.out.println("+                       + " );
+		System.out.println("+                       + " );
+		System.out.println("+                       + " );
+		System.out.println("+                       + " );
+		System.out.println("+                       + " );
+		System.out.println("+                       + " );
+		System.out.println("+                       + " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		
+	}
+}
