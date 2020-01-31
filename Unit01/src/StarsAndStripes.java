be1d90d8ca0b7852f966c818b2d8eadf7f133161
parasellia7975 <pathulith@gmail.com>
2020-01-31 13:12:02 -0800

    Finished Unit01

---
+++ Unit01/src/StarsAndStripes.java
@@ -0,0 +1,61 @@
+//© A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+public class StarsAndStripes
+{
+
+	
+   public StarsAndStripes()
+   {
+      out.println("StarsAndStripes");
+      printTwoBlankLines();
+   }
+
+   public void printTwentyStars()
+   {
+	  out.println("********************");
+	
+   }
+
+   public void printTwentyDashes()
+   {
+	   out.println("--------------------");
+	
+   }
+
+   public void printTwoBlankLines()
+   {
+	   out.println("\n\n");
+	  
+   }
+   
+   public void printASmallBox()
+   {	
+	     out.println("********************");
+	     out.println("--------------------");
+	     out.println("********************");
+	     out.println("--------------------");
+   }
+ 
+   public void printABigBox()
+   { 	
+	     out.println("********************");
+	     out.println("--------------------");
+	     out.println("********************");
+	     out.println("--------------------");
+	     out.println("********************");
+	     out.println("--------------------");
+	     out.println("********************");
+	     out.println("--------------------");
+	   
+   }   
+   
+}
+
---
+++ Unit01/src/StarsRunner.java
@@ -0,0 +1,26 @@
+//© A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+public class StarsRunner
+{
+   public static void main(String args[])
+   {
+	   
+      //instantiate a StarsAndStripes object
+      StarsAndStripes banner = new StarsAndStripes();
+      banner.printASmallBox();
+      banner.printTwoBlankLines();
+      banner.printABigBox();
+      
+      
+      //call the methods needed to make the patterns on the word document
+      
+   }
+}
