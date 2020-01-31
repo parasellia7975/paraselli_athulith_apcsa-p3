0e2fa2fc4754c9ac626fa4795c4becf3570727b7
parasellia7975 <pathulith@gmail.com>
2020-01-31 12:32:33 -0800

    finished variables Java

--- Unit00/src/Variables.java
+++ Unit00/src/Variables.java
@@ -18,15 +18,22 @@ public class Variables
 
 		//integer variables
 		byte byteOne = 127;
+		short shortOne = 7;
+		int intOne = 345435;
+		long longOne = 35654653;
 
 
 		//decimal variables
-
+		float floatOne = 2.5f;
+		double doubleOne = 535.345;
 
 		//other integer types
 
 
 		//other types
+		char charOne = 'a';
+		boolean boolOne = true;
+		String strOne = "ThisWord";
 
 
 		//output your information here
@@ -35,6 +42,25 @@ public class Variables
 		System.out.println("*        integer types          *");
 		System.out.println("*                               *");
 		System.out.println("*8 bit - byteOne = "+byteOne+"\t\t*");
+		System.out.println("*16 bit - ShortOne = "+shortOne+"\t\t*");
+		System.out.println("*32 bit - intOne = "+intOne+"\t\t*");
+		System.out.println("*64 bit - longOne = "+longOne+"\t\t*");
+		System.out.println("*                               *");
+		System.out.println("*        decimal types          *");
+		System.out.println("*                               *");
+		System.out.println("*32 bit - floatOne = "+floatOne+"\t\t*");
+		System.out.println("*64 bit - doubleOne = "+doubleOne+"\t\t*");
+		System.out.println("*                               *");
+		System.out.println("*        other types            *");
+		System.out.println("*                               *");
+		System.out.println("*8 bit - charOne = "+charOne+"\t\t*");
+		System.out.println("*1 bit - boolOne = "+boolOne+"\t\t*");
+		System.out.println("*176 bit - StringOne = "+strOne+"\t\t*");
+		System.out.println("*                               *");
+		System.out.println("/////////////////////////////////");
+		
+		
+		
 
 
 
