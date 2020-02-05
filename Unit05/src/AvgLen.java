b2a0afac4d99eab68f7aa2b10920489e0f61bcc4
parasellia7975 <pathulith@gmail.com>
2020-02-05 13:33:30 -0800

    added Uni05 and finished avrg Length

---
+++ Unit05/src/AvgLen.java
@@ -0,0 +1,15 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+//Date -
+
+public class AvgLen
+{
+   public static double go( String a, String b )
+	{
+	   double avrg;
+	   avrg = a.length() + b.length();
+	   avrg = avrg / 2;
+		return avrg;
+	}
+}
