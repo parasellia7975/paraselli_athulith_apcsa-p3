5b36d55c9ad391eca0c5df895f1802a59f9a3ea2
parasellia7975 <pathulith@gmail.com>
2020-02-06 13:22:04 -0800

    finsihed part of StringOddOrEven

--- Unit05/src/StringOddOrEven.java
+++ Unit05/src/StringOddOrEven.java
@@ -15,18 +15,34 @@ public class StringOddOrEven
 
 	public StringOddOrEven()
 	{
+		if(word.length()%2 == 0) {
+			out.println("It is Even");
+		}
+		else {
+			out.println("It is Odd");
+		}
 	}
 
 	public StringOddOrEven(String s)
 	{
+		if(s.length()%2 == 0) {
+			out.println("It is Even");
+		}
+		else {
+			out.println("It is Odd");
+		}
 	}
 
 	public void setString(String s)
 	{
+		word = s;
 	}
 
  	public boolean isEven()
  	{
+ 		if(word.length()%2 == 0) {
+ 			return true;
+ 		}
 		return false;
 	}
 
--- Unit05/src/StringRunner.java
+++ Unit05/src/StringRunner.java
@@ -11,7 +11,16 @@ import java.util.Scanner;
 
 public class StringRunner
 {
+	static Scanner keyboard = new Scanner(System.in);
 	public static void main ( String[] args )
 	{
+		out.println("Enter a word :: ");
+		String inputWord = "" + keyboard.nextLine();
+		StringOddOrEven theWord = new StringOddOrEven();
+		theWord.setString(inputWord);
+		theWord.isEven();
+		
+		
+		
 	}
 }
