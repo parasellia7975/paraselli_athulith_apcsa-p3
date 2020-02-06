0d8a7e8bd169935cef22d748251eaede2bf4b4a2
parasellia7975 <pathulith@gmail.com>
2020-02-06 12:48:00 -0800

    finished first last vowel

--- Unit05/src/FirstLastVowel.java
+++ Unit05/src/FirstLastVowel.java
@@ -8,6 +8,17 @@ public class FirstLastVowel
 {
    public static String go( String a )
 	{
-		return "add code here";
+	   String firstLetter = "" + a.charAt(0);
+	   String lastLetter = "" + a.charAt(a.length()-1);
+	   firstLetter = firstLetter.toUpperCase();
+	   lastLetter = lastLetter.toUpperCase();
+	   
+	   if(firstLetter.equals("A") || firstLetter.equals("E") || firstLetter.equals("I") || firstLetter.equals("O") || firstLetter.equals("I")){
+		   return "yes";
+	   }
+	   if(lastLetter.equals("A") || lastLetter.equals("E") || lastLetter.equals("I") || lastLetter.equals("O") || lastLetter.equals("I")){
+		   return "yes";
+	   }
+		return "no";
 	}
 }
