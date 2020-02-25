a319fbbd7265cfc7d9dbad05972b74f98b4c2935
parasellia7975 <pathulith@gmail.com>
2020-02-24 19:45:53 -0800

    Finished Triples (Finished unit 07)

--- Unit07/src/Triples.java
+++ Unit07/src/Triples.java
@@ -27,27 +27,36 @@ public class Triples
 	
 	private int greatestCommonFactor(int a, int b, int c)
 	{
-	for(int A = 0; A< number; A++) {
-		for (int B = 0; B< number; B++) {
-			for(int C = 0; C< number; C++) {
-				if()
-			}
-		}
-	}
 		int max = 0;
+		int smallest;
+		if(a<b && a<c)
+			smallest = a;
+			else if(b<a && c<c){
+				smallest = b;
+			}
+			else
+				smallest = c;
+		
+	for(int i = 1; i<= smallest; i++) {
+		if(a%i == 0 && b%i == 0 && c%i == 0) {
+		max = i;
+	}
+	}	
 
 
 
-		return 1;
+		return max;
 	}
 
 	public String toString()
 	{
-		String output="";
-		for(int A = 0; A< number; A++) {
-			for (int B = 0; B< number; B++) {
-				for (int C = 0; C< number; C++) {
-					if((A*A) + (B*B) == (C*C) && (greatestCommonFactor == 1) && ((A%2 == 0) && (B%2 == 1) || (A%2 ==1) && (B%2 == 0))) {
+		String output="The numbers are \n";
+		
+		for(int A = 1; A< number; A++) {
+			for (int B = 1; B< number; B++) {
+				for (int C = 1; C< number; C++) {
+					if(  ((A*A) + (B*B)) == (C*C) && (greatestCommonFactor(A, B, C) == 1) && ((A%2 == 0) && (B%2 == 1) || (A%2 ==1) && (B%2 == 0))    ) {
+						output = output + A + ", " + B + ", " + C + "\n";
 						
 					}
 				}
--- Unit07/src/TriplesRunner.java
+++ Unit07/src/TriplesRunner.java
@@ -4,9 +4,16 @@
 
 import static java.lang.System.*;
 
+import java.util.Scanner;
+
 public class TriplesRunner
 {
    public static void main(String args[])
    {
+	   Scanner keyboard = new Scanner(System.in);
+	   System.out.println("Enter an integer :: ");
+	   int num = keyboard.nextInt();
+	   Triples test = new Triples(num);
+	   System.out.println(test);
 	}
 }
