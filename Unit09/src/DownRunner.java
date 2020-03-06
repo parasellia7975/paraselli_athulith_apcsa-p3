d4e5ae9e7372f003e81e5938659be4edc54724d1
parasellia7975 <pathulith@gmail.com>
2020-03-06 12:22:53 -0800

    finished unit 9 activitives

--- Unit09/src/DownRunner.java
+++ Unit09/src/DownRunner.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 //(c) A+ Computer Science
 //www.apluscompsci.com
 //Name -
@@ -6,6 +8,16 @@
 public class DownRunner
 {
 	public static void main( String args[] )
-	{											
+	{		
+		ArrayList<Integer> test = new ArrayList<Integer>();
+		test.add(-99);
+		test.add(12);
+		test.add(4);
+		System.out.println(ListDown.go(test));
+		ArrayList<Integer> test2 = new ArrayList<Integer>();
+		test2.add(10);
+		test2.add(7);
+		test2.add(2);
+		System.out.println(ListDown.go(test2));
 	}
 }
