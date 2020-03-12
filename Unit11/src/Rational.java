fa880e00e3ab46bd3b738d62d81b8c49c5b79ae3
parasellia7975 <pathulith@gmail.com>
2020-03-12 13:13:57 -0700

    finished with Unit11

--- Unit11/src/Rational.java
+++ Unit11/src/Rational.java
@@ -13,8 +13,8 @@ class Rational implements Comparable<Rational>
 	
 	//write two constructors
 	public Rational() {
-		setNum(0);
-		setDen(0);
+		setNum(1);
+		setDen(1);
 	}
 	public Rational(int top, int bot) {
 		setNum(top);
@@ -23,6 +23,10 @@ class Rational implements Comparable<Rational>
 
 
 	//write a setRational method
+	public void setRational(int top, int bot) {
+		num = top;
+		den = bot;
+	}
 	
 
 	//write  a set method for numerator and denominator
@@ -35,7 +39,9 @@ class Rational implements Comparable<Rational>
 	
 	public void add(Rational  other)
 	{
-		
+		num = (num * other.returnDen()) + (other.returnNum() * den);
+		den = den * other.returnDen();
+		this.reduce();
 		//num1/den1 + num2/den2 
 		//new numerator = (num1 * den2 + num2 * den1)
 		//new denominator = (den1 * den2)
@@ -48,21 +54,33 @@ class Rational implements Comparable<Rational>
 
 	private void reduce()
 	{
-
+		int big = num;
+		if(den>num)
+			big = den;
+		for(int i = 1; i<=big; i++) {
+			if(num%i == 0 && den%i == 0) {
+				num = num/i;
+				den = den/i;
+			}
+		}
 
 
 	}
 
 	private int gcd(int numOne, int numTwo)
 	{
+		if(numOne>numTwo)
+			return numOne;
+		if(numOne == numTwo)
+			return 1;
+		return numTwo;
 
-
-		return 1;
+		
 	}
 
-	public Object clone ()
+	public Rational clone ()
 	{
-		return "";
+		return new Rational(num , den);
 	}
 
 
@@ -74,24 +92,38 @@ class Rational implements Comparable<Rational>
 		return den;
 	}
 	//write get methods for numerator and denominator
-	
+	public String returnRational() {
+		return "" + num + "\t" + den;
+	}
 	
 	public boolean equals( Object obj)
 	{
-
-
+		this.reduce();
+		if(obj == this)
+			return true;
 		return false;
+
+	
 	}
 
 	public int compareTo(Rational other)
 	{
-
-
-		return -1;
+		double decimal1 = 0;
+		double decimal2 = 0;
+		decimal1 = (double) num/den;
+		decimal2 = (double) other.returnNum()/other.returnDen();
+		if(decimal1 > decimal2)
+			return 1;
+		else if (decimal2 > decimal1) 
+			return -1;
+		else
+			return 0;
 	}
 
 
-
+	public String toString() {
+		return num + "/" + den;
+	}
 	
 	//write  toString() method
 	
