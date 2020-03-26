d0bae195eeafcede2750a7e7d2dda9d15bd89e1e
parasellia7975 <pathulith@gmail.com>
2020-03-26 10:09:06 -0700

    Finished monster

--- Unit12/src/MonsterRunner.java
+++ Unit12/src/MonsterRunner.java
@@ -12,11 +12,25 @@ public class MonsterRunner
 		Scanner keyboard = new Scanner(System.in);
 		
 		//ask for name and size
-		
+		String name = "";
+		int size = 0;
+		System.out.println("Enter a name ");
+		name = keyboard.nextLine();
+		System.out.println("Enter a size ");
+		size = keyboard.nextInt();
 		//instantiate monster one
+		Skeleton monsterOne = new Skeleton(name, size);
 		
 		//ask for name and size
+		System.out.println("Enter a name ");
+		name = keyboard.nextLine();
+		System.out.println("Enter a size ");
+		size = keyboard.nextInt();
 		
 		//instantiate monster two
+		Skeleton monsterTwo = new Skeleton(name, size);
+		
+		System.out.println(monsterOne);
+		System.out.println(monsterTwo);
 	}
 }
