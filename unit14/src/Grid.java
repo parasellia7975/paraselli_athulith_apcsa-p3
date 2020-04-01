c6a87b6d51d1901e6d2bda294c45e7d85df0c16d
parasellia7975 <pathulith@gmail.com>
2020-03-31 18:43:49 -0700

    Finsihed unit 14

--- unit14/src/Grid.java
+++ unit14/src/Grid.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 //(c) A+ Computer Science
 //www.apluscompsci.com
 //Name -
@@ -9,24 +11,58 @@ public class Grid
    //load vals into the rows x cols grid randomly
 	public Grid(int rows, int cols, String[] vals)
 	{
+		grid = new String[rows][cols];
+		for(int i = 0; i<cols; i++) {
+			for(int k = 0; k<rows; k++) {
+				grid[k][i] = vals[(int) (Math.random()*(vals.length))];
+			}
+		}
 	}
 
 	//find out which of the vals occurs the most
 	public String findMax(String[] vals)
 	{
-		return "nothing yet";
+		ArrayList<Integer> word = new ArrayList<Integer>();
+		boolean mostCommon = true;
+		String commonWord = "nothing yet";
+		for(int i = 0; i<grid.length; i++) {
+			for(int k = 0; k<grid[i].length; k++) {
+				word.add(countVals(grid[i][k]));
+				for(int j = 0; j<word.size(); j++) {
+					if(word.get(j)>word.get(word.size()-1)) {
+						mostCommon = false;
+					}
+				}
+				if(mostCommon)
+					commonWord = grid[i][k];
+			}
+		}
+		return commonWord + " appears the most";
 	}
 
 	//returns a count of how many times val occurs in the matrix
 	private int countVals( String val )
 	{
-		return 0;
+		int count = 0;
+		for(int i = 0; i< grid.length; i++) {
+			for(int k = 0; k<grid[i].length; k++) {
+				if(grid[i][k].equals(val)) {
+					count = count + 1;
+				}
+			}
+		}
+		return count;
 	}
 
 	//display the grid
 	public String toString()
 	{
 		String output="";
+		for(int i = 0; i< grid.length; i++) {
+			for(int k = 0; k<grid[i].length; k++) {
+				output = output + " " + grid[i][k];
+			}
+		}
 		return output;
 	}
 }
--- unit14/src/GridRunner.java
+++ unit14/src/GridRunner.java
@@ -11,5 +11,10 @@ public class GridRunner
 {
 	public static void main( String args[] ) throws IOException
 	{
+		String[] stringTest = {"a","x","2","f"};
+		Grid test = new Grid(10,10, stringTest);
+		System.out.println(test);
+		System.out.println(test.findMax(stringTest));
+		
 	}
 }
