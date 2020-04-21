4c137a54a36f9f64bb4f8f8d3be104375741f08d
parasellia7975 <pathulith@gmail.com>
2020-04-21 11:27:36 -0700

    Finsihed WordSortTwo

--- Unit13/src/WordSortTwo.java
+++ Unit13/src/WordSortTwo.java
@@ -12,15 +12,40 @@ public class WordSortTwo
 
 	public WordSortTwo(String sentence)
 	{
+		int start = 0; 
+		int index = 0; 
+		char[] strArray = sentence.toCharArray();
+		for(int i = 0; i< strArray.length; i++) {
+			if(strArray[i] == ' ') {
+				wordRay[index] = sentence.substring(start,i);
+				index += 1;
+				start = i + 1;
+			}
+		}
+		
 	}
 
 	public void sort()
 	{
+		String tempString;
+		for(int i = 0; i<wordRay.length-1; i++) {
+			for(int k = 0; k<wordRay.length; k++) {
+				if(wordRay[k].compareTo(wordRay[k+1])<0) {
+					tempString = wordRay[k];
+					wordRay[k] = wordRay[k+1];
+					wordRay[k+1] = tempString;
+				}
+			}
+		}
 	}
 
 	public String toString()
 	{
 		String output="";
+		for(int i = 0; i<wordRay.length; i ++) {
+			output = output + " " + wordRay[i];
+		}
+		
 		return output+"\n\n";
 	}
 }
